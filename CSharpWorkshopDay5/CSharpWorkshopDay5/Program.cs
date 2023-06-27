d656e3f24d80b56c0c790ac028cbb6cd15e02170
AndrewPlate <nationalbazil@gmail.com>
2023-06-27 12:04:02 +0100

    Class
    Class make code more efficient using functions

--- CSharpWorkshopDay5/CSharpWorkshopDay5/Program.cs
+++ CSharpWorkshopDay5/CSharpWorkshopDay5/Program.cs
@@ -10,13 +10,56 @@ BruceWayne.NameOfStudent = "Bruce Wayne";
 BruceWayne.StudyHoursPerWeek = 20;
 BruceWayne.StudyWeeksCompleted = 0;
 
+BruceWayne.NameOfStudent = "Bruce Wayne";
+BruceWayne.StudyHoursPerWeek = 20;
+BruceWayne.StudyWeeksCompleted = 0;
+
+BruceWayne.BuildDisplayStudentInformation();
+BruceWayne.DisplayStudentInformation();
+
 SelinaKyle.NameOfStudent = "Selina Kyle";
 SelinaKyle.StudyHoursPerWeek = 10;
 SelinaKyle.StudyWeeksCompleted = 10;
 
-string outputForBatman = $" Student Name : {BruceWayne.NameOfStudent} Study Hours : {BruceWayne.StudyHoursPerWeek} Number of Weeks Completed : {BruceWayne.StudyWeeksCompleted}";
+SelinaKyle.NameOfStudent = "Selina Kyle";
+SelinaKyle.StudyHoursPerWeek = 10;
+SelinaKyle.StudyWeeksCompleted = 10;
 
-string outputForCatwoman = $" Student Name : {SelinaKyle.NameOfStudent} Study Hours : {SelinaKyle.StudyHoursPerWeek} Number of Weeks Completed : {SelinaKyle.StudyWeeksCompleted}";
+SelinaKyle.BuildDisplayStudentInformation();
+SelinaKyle.DisplayStudentInformation();
+#region
+//string outputForBatman = $" Student Name : {BruceWayne.NameOfStudent} Study Hours : {BruceWayne.StudyHoursPerWeek} Number of Weeks Completed : {BruceWayne.StudyWeeksCompleted}";
 
-Console.WriteLine(outputForBatman);
-Console.WriteLine(outputForCatwoman);
+//string outputForCatwoman = $" Student Name : {SelinaKyle.NameOfStudent} Study Hours : {SelinaKyle.StudyHoursPerWeek} Number of Weeks Completed : {SelinaKyle.StudyWeeksCompleted}";
+
+//Console.WriteLine(outputForBatman);
+//Console.WriteLine(outputForCatwoman);
+#endregion
+
+
+//Super hero guy called the phantom 
+
+Student KitWalker = new Student();
+KitWalker.NameOfStudent = "KitWalker";
+KitWalker.StudyHoursPerWeek= 30;
+KitWalker.StudyWeeksCompleted = 30;
+
+KitWalker.BuildDisplayStudentInformation();
+KitWalker.DisplayStudentInformation();
+
+//500 hours is the amount of time it takers to become a begginer developer 
+
+int TotalNumberOfHoursToBecomeDevelepor = 500;
+
+//20 hours per week
+// 25 weeks to become a beginner developer 
+//number of weeks already completed is 0
+// remaining 25 - 0 = 25 weeks
+// batman will become a develepor in 25 weeks 
+
+
+// 10 hours per week
+// 50 weeks to become a beginner developer 
+// number of weeks already completed is 10 weeks
+// remaining 50 - 10 = 40 weeks
+// catwomen will be a developer in 40 weeks 
--- CSharpWorkshopDay5/CSharpWorkshopDay5/Student.cs
+++ CSharpWorkshopDay5/CSharpWorkshopDay5/Student.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,10 +9,27 @@ namespace CSharpWorkshopDay5
 {
     public class Student
     {
+        // fields or properties 
         public int StudyHoursPerWeek { get; set; }
 
         public int StudyWeeksCompleted { get; set; }
 
         public string NameOfStudent { get; set; }
+        
+        // For The sake of the output
+        public string OutPutStorage { get; set; }   
+
+        // Functions or methods or behaviours or actions
+
+        public void BuildDisplayStudentInformation() 
+        {
+            this.OutPutStorage = $" Student Name : {this.NameOfStudent} Study Hours : {this.StudyHoursPerWeek} Number of Weeks Completed : {this.StudyWeeksCompleted}";
+
+        }
+        
+        public void DisplayStudentInformation() 
+        {
+            Console.WriteLine(this.OutPutStorage);
+        }
     }
 }
