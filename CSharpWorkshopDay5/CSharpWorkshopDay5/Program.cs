96103497979e94ccee649977708d38e0bb2d1d43
AndrewPlate <nationalbazil@gmail.com>
2023-06-27 11:31:19 +0100

    Classes
    Classes

---
+++ CSharpWorkshopDay5/CSharpWorkshopDay5/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CSharpWorkshopDay5;
+
+//student called Bruce Wayne 
+
+Student BruceWayne = new Student();
+Student SelinaKyle = new Student();
+
+BruceWayne.NameOfStudent = "Bruce Wayne";
+BruceWayne.StudyHoursPerWeek = 20;
+BruceWayne.StudyWeeksCompleted = 0;
+
+SelinaKyle.NameOfStudent = "Selina Kyle";
+SelinaKyle.StudyHoursPerWeek = 10;
+SelinaKyle.StudyWeeksCompleted = 10;
+
+string outputForBatman = $" Student Name : {BruceWayne.NameOfStudent} Study Hours : {BruceWayne.StudyHoursPerWeek} Number of Weeks Completed : {BruceWayne.StudyWeeksCompleted}";
+
+string outputForCatwoman = $" Student Name : {SelinaKyle.NameOfStudent} Study Hours : {SelinaKyle.StudyHoursPerWeek} Number of Weeks Completed : {SelinaKyle.StudyWeeksCompleted}";
+
+Console.WriteLine(outputForBatman);
+Console.WriteLine(outputForCatwoman);
---
+++ CSharpWorkshopDay5/CSharpWorkshopDay5/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpWorkshopDay5
+{
+    public class Student
+    {
+        public int StudyHoursPerWeek { get; set; }
+
+        public int StudyWeeksCompleted { get; set; }
+
+        public string NameOfStudent { get; set; }
+    }
+}
